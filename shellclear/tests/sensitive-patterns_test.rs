3bae2cf672df0ab9f5dc4fe26009d3519d53823e
Elad Kaplan <kaplan.elad@gmail.com>
2022-09-03 21:53:23 +0300

    add test for sensitive data patterns

--- shellclear/tests/sensitive-patterns_test.rs
+++ shellclear/tests/sensitive-patterns_test.rs
@@ -14,6 +14,7 @@ struct TestSensitivePatterns {
 #[derive(Debug)]
 struct TestPatternResult {
     name: String,
+    test: String,
     detect: String,
     expected: String,
 }
@@ -25,20 +26,27 @@ fn can_detect_regex_sensitive_patterns() {
     for pattern in &patterns {
         let pattern_id = pattern.id.to_lowercase().replace(" ", "_");
         let pattern_file = format!("tests/suites_sensitive_patterns/{}.yaml", pattern_id);
-        if pattern_id == "aws_cred" {
-            let f = std::fs::File::open(pattern_file).unwrap();
-            let tests: Vec<TestSensitivePatterns> = serde_yaml::from_reader(f).unwrap();
-
-            let mut results = Vec::new();
-            for test in tests {
-                let caps = pattern.test.captures(&test.test).unwrap();
-                results.push(TestPatternResult {
-                    name: test.name,
-                    detect: caps.get(2).map_or("", |m| m.as_str()).to_string(),
-                    expected: test.expected,
-                })
+        let f = match std::fs::File::open(pattern_file) {
+            Ok(f) => f,
+            Err(_e) => {
+                println!("not found tests for {}", pattern.id);
+                return assert!(false);
             }
-            assert_debug_snapshot!(pattern_id, results);
+        };
+        let tests: Vec<TestSensitivePatterns> = serde_yaml::from_reader(f).unwrap();
+        let mut results = Vec::new();
+        for test in tests {
+            let caps = pattern.test.captures(&test.test).unwrap();
+            results.push(TestPatternResult {
+                name: test.name,
+                test: test.test.clone(),
+                detect: caps
+                    .get(pattern.secret_group as usize)
+                    .map_or("", |m| m.as_str())
+                    .to_string(),
+                expected: test.expected,
+            })
         }
+        assert_debug_snapshot!(pattern_id, results);
     }
 }
