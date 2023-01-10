5865562e56558a2f6cd08c3e7da89b8bb7390dcf
Arnovsky <alex190030@gmail.com>
2023-01-10 16:13:08 +0200

    Updates docs for masking feature, added two missing test cases (#71)
    * updates docs for masking feature, added two missing test cases.
    
    * module import for anyhow in test

--- shellclear/src/masker.rs
+++ shellclear/src/masker.rs
@@ -34,12 +34,14 @@ impl Masker {
                 )
                 .mask();
 
-                // there can still be an false-positive here, if the text is all made up of asterisks
+                // there can still be an false-positive here, if the text is all made up of
+                // asterisks
                 !command.command.contains(&replaced_secret)
             });
 
             if !should_mask {
-                // If all the secrets are already masked, set the detections to an empty vec, this represents that this command is already masked / no secrets detected
+                // If all the secrets are already masked, set the detections to an empty vec,
+                // this represents that this command is already masked / no secrets detected
                 command.detections = vec![];
                 continue;
             }
@@ -65,5 +67,62 @@ impl Masker {
             .iter_mut()
             .filter(|c| !c.detections.is_empty())
             .collect::<Vec<_>>()
+    }
+}
+
+#[cfg(test)]
+mod test_masker {
+    use anyhow::Result;
+    use insta::assert_debug_snapshot;
+    use regex::Regex;
+
+    use crate::{
+        data::{Command, Detection},
+        masker::Masker,
+        shell::Shell::Zshrc,
+    };
+
+    #[test]
+    fn mask_results() -> Result<()> {
+        let mut commands = vec![Command {
+            shell_type: Zshrc,
+            detections: vec![Detection {
+                test: Regex::new("export (MASK_ME)")?,
+                name: "mask me mock".to_string(),
+                secret_group: 1,
+                id: "".to_string(),
+            }],
+            command: "export MASK_ME".to_string(),
+            data: "export MASK_ME".to_string(),
+            secrets: vec!["MASK_ME".to_string()],
+        }];
+
+        Masker::new().mask_sensitive_findings(commands.as_mut());
+
+        assert_debug_snapshot!(commands);
+
+        Ok(())
+    }
+
+    #[test]
+    fn remove_already_masked_detection() -> Result<()> {
+        let mut commands = [Command {
+            shell_type: Zshrc,
+            detections: vec![Detection {
+                test: Regex::new("export (MASK_ME)")?,
+                name: "mask me mock".to_string(),
+                secret_group: 1,
+                id: "".to_string(),
+            }],
+            command: "export MA*****".to_string(),
+            data: "export MA*****".to_string(),
+            secrets: vec!["MASK_ME".to_string()],
+        }];
+
+        Masker::new().mask_sensitive_findings(commands.as_mut());
+
+        assert_debug_snapshot!(commands);
+
+        Ok(())
     }
 }
